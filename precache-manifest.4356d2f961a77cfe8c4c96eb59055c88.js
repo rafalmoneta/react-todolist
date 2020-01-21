7ae537950ff746bb26c4a449a0a34c4b8422fc17
Rafal Moneta <rafalmoneta5@gmail.com>
2020-01-21 10:27:00 +0100

    Updates

---
+++ precache-manifest.4356d2f961a77cfe8c4c96eb59055c88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b1344b4d02945f5271662c85a7923a",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "b2d919dd461c587eddef",
+    "url": "/react-todolist/static/css/main.e404055d.chunk.css"
+  },
+  {
+    "revision": "0fcb299961c8769e648e",
+    "url": "/react-todolist/static/js/2.37aa40f8.chunk.js"
+  },
+  {
+    "revision": "b2d919dd461c587eddef",
+    "url": "/react-todolist/static/js/main.d43be0eb.chunk.js"
+  },
+  {
+    "revision": "0e5583f66e0a8eef4a46",
+    "url": "/react-todolist/static/js/runtime-main.3243174c.js"
+  }
+]);
